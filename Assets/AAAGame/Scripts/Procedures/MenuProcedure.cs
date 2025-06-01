83b96e8caa6b33421eeffbb0ee13cd666da4b89c
final5788 <final5788@noreply.gitcode.com>
2025-06-01 11:35:04 +0800

    update

--- Assets/AAAGame/Scripts/Procedures/MenuProcedure.cs
+++ Assets/AAAGame/Scripts/Procedures/MenuProcedure.cs
@@ -10,6 +10,6 @@ public class MenuProcedure : ProcedureBase
     protected override void OnEnter(IFsm<IProcedureManager> procedureOwner)
     {
         base.OnEnter(procedureOwner);
-        GF.LogInfo("这是一个GF_X空白工程模板");
+        GF.Log("这是一个GF_X空白工程模板");
     }
 }
